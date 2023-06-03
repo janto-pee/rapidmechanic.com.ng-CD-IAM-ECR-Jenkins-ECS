818c35952fc58dd5eafcaa3f212b97851937aa4c
ayobami <ayobami_adejumo@yahoo.com>
2023-06-03 06:27:43 -0700

    clean up

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,12 +1,10 @@
 import express, {Request, Response} from "express";
 import userRouter from './user.routes';
 import sessionRouter from './session.routes'
-import vehicleRouter from './vehicle.routes'
 
 const router = express.Router()
 router.use(userRouter)
 router.use(sessionRouter)
-router.use(vehicleRouter)
 
 router.get('/api', (req: Request, res: Response) => {
     res.json({data: 'all good'})
